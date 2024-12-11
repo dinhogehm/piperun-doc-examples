07cec1daf2131bc924616aa28a06aa842cb7b037
carinaayres <carina.ayres@crmpiperun.com>
2024-12-11 15:02:57 -0300

    Create 172-bloco-4.js

---
+++ 172-bloco-4.js
@@ -0,0 +1,17 @@
+let rules = {
+            "update": true,
+            "status": "open",
+            "equal_pipeline": true,
+            "filter_status_update": "open"
+        };
+
+        let lead = [{
+            "id": email,
+            "title": assunto,
+            "company": company,
+            "name": name,
+            "email": email,
+            "notes": [
+                    "Mensagem: " + mensagem
+                     ]
+        }];

72bd56d6def14c16c2ab8602300d86b3219c0528
Osvaldo Gehm <osvaldo@odig.net>
2024-12-11 12:54:25 -0300

    Create 172-bloco-2.js

---
+++ 172-bloco-2.js
@@ -0,0 +1,11 @@
+function enviarDados() {
+
+  let endpointHash = "https://app.pipe.run/webservice/integradorJson?hash=[SUA-HASH-AQUI]";
+
+  let name = document.getElementById("idName").value;
+  let company = document.getElementById("idCompany").value;
+  let email = document.getElementById("idEmail").value;
+  let assunto = document.getElementById("idAssunto").value;
+  let mensagem = document.getElementById("idMsg").value;
+
+}

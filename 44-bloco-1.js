ee7abab54a37e767ef056a61b732da030d1fd06b
carinaayres <carina.ayres@crmpiperun.com>
2024-12-20 15:05:36 -0300

    44-bloco-1.js

---
+++ 44-bloco-1.js
@@ -0,0 +1,24 @@
+array(
+    'conversion_identifier' => 'Identificador da ação',
+    'traffic_source' => 'Origem da OP',
+    'traffic_medium' => 'referral',
+    'tags' => array(
+        0 => 'Tag 1 definida na ação',
+        1 => 'Tag 2 definida na ação',
+        2 => 'Produto 1 da Oportunidade',
+        3 => 'Produto 2 da Oportunidade',
+    ),
+    'name' => 'Nome do contato',
+    'email' => 'emaildocontato@email.com',
+    'personal_phone' => '5199999999',
+    'city' => 'Cidade do contato',
+    'state' => 'Estado do contato',
+    'company_name' => 'Empresa do contato',
+    'company_site' => 'Website da empresa do contato',
+    'company_adress' => 'Endereço da empresa do contato',
+),
+
+array(
+    'opportunity' => true, (caso seja marcado como oportunidade no RD)
+    'contact_owner_email' => 'emaildousuariopiperun@email.com',
+)      
